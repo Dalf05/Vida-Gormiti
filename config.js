32eb916b5b6ba58295c95607bcb412f6c95fccad
Dalf05 <danielmolinoferron@gmail.com>
2025-03-27 14:48:25 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 export default {
     loadingTime: 5000, // Time in milliseconds
-    password: 'lolcol'
+    passwordHash: '4d186321c1a7f0f354b297e8914ab240', // Hashed version of "lolcol" (MD5 of "lolcol")
+    salt: '' // Empty salt for now to make it work with both methods
 };
